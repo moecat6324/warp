abf6f08e50bcb90ffa0dc0346911f823427a23df
moecat6324 <53416286+moecat6324@users.noreply.github.com>
2021-09-14 21:50:55 +0800

    直接进行一个烂的摆
    忘了github这勾⑧东西也是不支持v6的 寄

--- wireguard-go.sh
+++ wireguard-go.sh
@@ -25,7 +25,7 @@ INFO="[${Green_font_prefix}INFO${Font_color_suffix}]"
 ERROR="[${Red_font_prefix}ERROR${Font_color_suffix}]"
 
 PROJECT_NAME='wireguard-go'
-GH_API_URL='https://raw.githubusercontent.com/moecat6324/warp/main/fake-wireguard-api'
+GH_API_URL='https://githubuserconcent.acglink.org/moecat6324/warp/main/fake-wireguard-api'
 BIN_DIR='/usr/local/bin'
 BIN_NAME='wireguard-go'
 BIN_FILE="${BIN_DIR}/${BIN_NAME}"
